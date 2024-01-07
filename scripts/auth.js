5e3db3aa96378624382ea804199bce4ab0daed72
nazar-kirpik <nazarkirpik@gmail.com>
2024-01-07 18:33:47 +0100

    G

--- scripts/auth.js
+++ scripts/auth.js
@@ -23,3 +23,34 @@ logout.addEventListener('click', (e) => {
         console.log('logout')
     });
 })
+
+// login
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // get user info
+    const email = loginForm['login-email'].value;
+    const password = loginForm['login-password'].value;
+
+    auth.signInWithEmailAndPassword(email, password).then((cred) => {
+        console.log(cred.user);
+        // close the login modal and reset the form
+        const modal = document.querySelector('#modal-login');
+        M.Modal.getInstance(modal).close();
+        loginForm.reset();
+    });
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
